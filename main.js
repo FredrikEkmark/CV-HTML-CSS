598188b9e9da86f270c50fe852d328714737a5ed
FredrikEkmark <fredrik.ekmark@gmail.com>
2023-03-07 20:57:43 +0100

    Finished version 1.0

---
+++ main.js
@@ -0,0 +1,14 @@
+"use strict";
+
+function a4Formating() {
+    document.getElementById("mainColumn").classList.add("a4Format");
+    document.getElementById("sideBar").classList.add("a4Format");
+    document.getElementById("body").classList.add("a4Format");
+    document.getElementById("main").classList.add("a4Format");
+    document.getElementById("header").classList.add("a4Format");
+    document.getElementById("linksDiv").classList.add("a4Format");
+    document.getElementById("gitHubLink").innerHTML = "github.com/FredrikEkmark";
+    document.getElementById("linkedInLink").innerHTML = "se.linkedin.com/in/fredrik-ekmark-21255012a";
+    document.getElementById("header").innerHTML = "";
+};
+
