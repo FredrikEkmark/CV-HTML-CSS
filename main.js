aae04d49f047914e14fa81798207f7e733f19bbf
FredrikEkmark <fredrik.ekmark@gmail.com>
2023-03-08 11:08:09 +0100

    update to a4Format bug with links

--- main.js
+++ main.js
@@ -7,11 +7,12 @@ function a4Formating() {
     document.getElementById("main").classList.add("a4Format");
     document.getElementById("header").classList.add("a4Format");
     document.getElementById("skillsList").classList.add("a4Format");
-    document.getElementById("linksDiv").classList.add("a4Format");
     document.getElementById("linksDiv").innerHTML = `
         <p id="gitHubLink">github.com/FredrikEkmark</p>
         <p id="linkedInLink">se.linkedin.com/in/fredrik-ekmark-21255012a</p>
     `;
+    document.getElementById("linksDiv").classList.add("a4Format");
+    
     document.getElementById("contact").innerHTML = `
         <h2>Kontakt</h2>
         <p>Telefon:</p>
